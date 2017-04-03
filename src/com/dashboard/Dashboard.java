8367178a6cffdd57e477a649655e4b07dce58de2
abinkrishna <abinkrishna2004@gmail.com>
2017-04-02 21:53:36 -0400

    Signed-off-by: abinkrishna <abinkrishna2004@gmail.com>

--- src/com/dashboard/Dashboard.java
+++ src/com/dashboard/Dashboard.java
@@ -6,6 +6,8 @@ public class Dashboard {
 		//Init Graphs//		
 		System.out.println("Init Graph Module");
 		Thread.sleep(1000);
+		System.out.println("Init Graph ");
+		System.out.println("Init Graph ");
 	} 
 	
 	public void initCalculation(){
